4bcc5ed8f076b4ffab043a3fb4abc535d00db99c
rlyyah <39671388+rlyyah@users.noreply.github.com>
2020-09-28 16:54:17 +0200

    Adding condition into the list declaration

--- listComprehensions.py
+++ listComprehensions.py
@@ -1,13 +1,15 @@
 import os, glob
 
-#Great way to operate on lists
-
+# Great way to operate on lists
 a_list = [1,9,18,4]
 print(a_list)
 b_list = [num **2 for num in a_list]
 print(b_list)
 
-#Finding files of particular extension
+# Finding files of particular extension
 txt_file_path_list = [os.path.realpath(f) for f in glob.glob('*.txt')]
 print(txt_file_path_list)
 
+# You can even add a condition into the list
+txt_files_list_under_6000 = [f for f in glob.glob('*.txt') if os.stat(f).st_size < 6000]
+print(txt_file_path_list)
