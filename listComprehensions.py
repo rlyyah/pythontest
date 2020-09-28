f1c794c06f9977c7b4fb2593d1f0e26b29e578d2
rlyyah <39671388+rlyyah@users.noreply.github.com>
2020-09-28 16:48:48 +0200

    test after gitignore

--- listComprehensions.py
+++ listComprehensions.py
@@ -1,4 +1,13 @@
+import os, glob
+
+#Great way to operate on lists
+
 a_list = [1,9,18,4]
 print(a_list)
 b_list = [num **2 for num in a_list]
 print(b_list)
+
+#Finding files of particular extension
+txt_file_path_list = [os.path.realpath(f) for f in glob.glob('*.txt')]
+print(txt_file_path_list)
+
